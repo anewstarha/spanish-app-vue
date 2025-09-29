274bc8bed1089b1e38a0495e1458e98cd407585f
Longfei <chenlongfei@outlook.com>
2025-09-30 01:37:27 +0200

    APP notes

--- src/main.js
+++ src/main.js
@@ -3,13 +3,24 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia' // 1. 从 pinia 库导入 createPinia 函数
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+// 1. 导入 supabase 客户端
+import { supabase } from './supabase'
 
-app.use(createPinia()) // 2. 在应用中使用 Pinia
-app.use(router)
+let app;
 
-app.mount('#app')
+// 2. 将 app 的创建和挂载移入 onAuthStateChange 回调
+// 这个回调函数在页面加载时会立即被触发一次
+supabase.auth.onAuthStateChange(() => {
+
+  // 3. 使用一个技巧确保应用只被创建和挂载一次
+  if (!app) {
+    app = createApp(App)
+    app.use(createPinia())
+    app.use(router)
+    app.mount('#app')
+  }
+})
