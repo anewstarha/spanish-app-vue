ab0287cd4739bdadb28ac2d4bbc26689a7a98fee
Longfei <chenlongfei@outlook.com>
2025-09-30 01:06:51 +0200

    APP

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa' // 1. 导入插件
+import { VitePWA } from 'vite-plugin-pwa'
+import path from 'path' // 1. 导入 Node.js 的 path 模块
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  // 2. 添加 resolve 配置来定义路径别名
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+
+  // 3. 我们之前定义的 base 路径
+  base: '/spanish-app/',
+
   plugins: [
     vue(),
-    VitePWA({ // 2. 添加插件配置
+    // 4. 我们之前定义的 PWA 插件配置
+    VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.png', 'apple-touch-icon.png'],
       manifest: {
@@ -27,5 +40,5 @@ export default defineConfig({
         ]
       }
     })
-  ]
+  ],
 })
